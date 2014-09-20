2f86add53f02b5f352d23c12315d767713017605
SmartXiaoMing <95813422@qq.com>
2014-09-20 23:54:26 +0800

    update
    N/A
    
    update
    Signed-off-by: SmartXiaoMing <95813422@qq.com>

---
+++ js/data.js
@@ -0,0 +1,87 @@
+var RoleCards = [
+  {
+    name: "近卫",
+    image: ""
+  },
+  {
+    name: "天灾",
+    image: ""
+  },
+];
+
+var BloodCards = [
+  {
+    name: "6点",
+    image: ""
+  },
+  {
+    name: "8点",
+    image: ""
+  },
+  {
+    name: "10点",
+    image: ""
+  },
+  {
+    name: "12点",
+    image: ""
+  },
+];
+
+var MagicCards = [
+  {
+    name: "6点",
+    image: ""
+  },
+  {
+    name: "8点",
+    image: ""
+  },
+  {
+    name: "10点",
+    image: ""
+  },
+  {
+    name: "12点",
+    image: ""
+  },
+];
+
+var PositionCards = [
+  {
+    name: "12点",
+    image: ""
+  }
+];
+
+var BasicCards = [
+  {
+    name: "治疗药膏",
+    cost: 0,
+    image: "",
+    desc: "增加1点体力"
+  },
+  {
+    name: "净化药水",
+    cost: 0,
+    image: "",
+    desc: "增加1点魔力"
+  },
+  {
+    name: "速度之靴",
+    cost: 1,
+    image: "",
+    desc: "增加1点移动范围"
+  },
+];
+
+var CardSet = [
+  { number: "0", color: 0, name:""},
+  { number: "0", color: 0, name:""},
+  { number: "0", color: 0, name:""},
+  { number: "0", color: 0, name:""},
+  { number: "0", color: 0, name:""},
+  { number: "0", color: 0, name:""},
+  { number: "0", color: 0, name:""},
+];
+
